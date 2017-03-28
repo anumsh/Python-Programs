8ef10b2fb1fdcda81c433d614b14069be40f28de
anumsh <anumsharma09@gmail.com>
2017-03-28 13:37:09 -0700

    fix: fix the code

--- fibonacci.py
+++ fibonacci.py
@@ -1,28 +1,13 @@
-# Program to display the Fibonacci sequence up to n-th term where n is provided by the user
-
-
+# program for fibonacci series
 
 def fibonacci(n):
-    # first two terms
-    n1 = 0
-    n2 = 1
-    count = 2
-    if n <= 0:
-       print("Please enter a positive integer")
-    elif n == 1:
-       print("Fibonacci sequence ",n)
-       print(n1)
-    else:
-       print("Fibonacci sequence",n)
-       print(n1,",",n2)
-    while count < n:
-       n3= n1 + n2
-       # update values
-       n1 = n2
-       n2 = n3
-       count += 1
-       return n
-       
-    
-
-print fibonacci(10)
+  a=1
+  b=1 
+  for i in range(n):
+    temp=a 
+    a=b 
+    b = temp + a 
+    print str(a)
+  return a
+  
+print fibonacci(5)
