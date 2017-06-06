44b408da80563cd9ce341d34a6bf92c280ff3d67
anumsh <anumsharma09@gmail.com>
2017-06-05 23:56:41 -0700

    feat: new python program

---
+++ bank_account.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import sys
+"""
+Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following:
+D 100
+W 200
+¡­
+D means deposit while W means withdrawal.
+Suppose the following input is supplied to the program:
+D 300
+D 300
+W 200
+D 100
+Then, the output should be:
+500
+"""
+
+netAmount = 0
+while True:
+    s = raw_input()
+    if not s:
+        break
+    values = s.split(" ")
+    operation = values[0]
+    amount = int(values[1])
+    if operation=="D":
+        netAmount+=amount
+    elif operation=="W":
+        netAmount-=amount
+    else:
+        print(" no output")
+print netAmount
