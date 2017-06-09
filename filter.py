4d29d46f930d7c331e1b7a9784c2623e349f3449
anumsh <anumsharma09@gmail.com>
2017-06-08 23:56:42 -0700

    feat: Add more examples of filter()

--- filter.py
+++ filter.py
@@ -1,3 +1,14 @@
+# The built-in filter() function operates on any iterable type (list, tuple, string, etc).
+
+# Python Filter with Number
+numbers=[1, 6, 3, 8, 4, 9]
+
+def lessThanFive(element):
+           return element < 5
+
+print filter(lessThanFive, numbers)  # [1, 3, 4]
+
+
 a = [1,2,4,1,2,3]
 s = set(a)
 print s
@@ -15,3 +26,16 @@ when passed to the function check() which will check if the value
 is in the set, s. Since all the numbers in the set come from the
 values list, all of the original values in the list will return True.
 """
+
+# Python Filter with String
+names = ('angel', 'anushka', 'anum', '')
+
+print filter(None, names)  # ('angel', 'anushka', 'anum')
+
+#  Python Filter with a Function
+def startsWithA(element):
+           if len(element) > 0:
+                      return element[0] == 'a'
+           return False
+
+print filter(startsWithA, names) # ('angel', 'anushka', 'anum')
