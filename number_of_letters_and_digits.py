868873fb635fdbf0f5bde631e41f01c5c0e711c2
anumsh <anumsharma09@gmail.com>
2017-06-11 23:40:06 -0700

    feat: Calculate the number of letters and digits

---
+++ number_of_letters_and_digits.py
@@ -0,0 +1,20 @@
+"""
+Write a program that accepts a sentence and calculate the number of letters and digits.
+Suppose the following input is supplied to the program:
+hello world! 123
+Then, the output should be:
+LETTERS 10
+DIGITS 3
+"""
+
+s = raw_input("enter the sentence")
+d={"DIGITS":0, "LETTERS":0}
+for c in s:
+    if c.isdigit():
+        d["DIGITS"]+=1
+    elif c.isalpha():
+        d["LETTERS"]+=1
+    else:
+        pass
+print "LETTERS", d["LETTERS"]
+print "DIGITS", d["DIGITS"]
