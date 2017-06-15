96c6b926815fade2a5380e35a5352cdc5c7bf2c5
anumsh <anumsharma09@gmail.com>
2017-06-14 23:48:58 -0700

    feat: Program which accepts a sequence of comma seperated 4 digit binary numbers

---
+++ binary_numbers.py
@@ -0,0 +1,24 @@
+"""
+Write a program which accepts a sequence of comma separated 4 digit binary numbers
+as its input and then check whether they are divisible by 5 or not.
+The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+Example:
+0100,0011,1010,1001
+Then the output should be:
+1010
+"""
+value = []
+items=[x for x in raw_input().split(',')]
+for p in items:
+    intp = int(p, 2)
+    if not intp%5:
+        value.append(p)
+
+print ','.join(value)
+
+
+"""
+output:
+1011,1101,1010,1001
+1010
+"""
