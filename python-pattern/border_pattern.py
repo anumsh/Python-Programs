a14c3908221dcf46a1a94f1875badb06c3d3f8da
anumsh <anumsharma09@gmail.com>
2017-06-15 21:33:46 -0700

    feat: sqaure and border pattern in python

---
+++ python-pattern/border_pattern.py
@@ -0,0 +1,20 @@
+def borderPattern(size):
+  size = 5
+  inner_size = size - 2
+  print ('*' * size)
+  for i in range(inner_size):
+    print ('*' + '  ' * inner_size + '*')
+  print ('*' * size)
+  
+      
+print borderPattern(5)
+
+"""
+output:
+
+*****
+*   *
+*   *
+*   *
+*****
+"""
---
+++ python-pattern/square_pattern.py
@@ -0,0 +1,18 @@
+def squarePattern(n):
+  for i in range(n):
+    print "*" * n
+  
+      
+print squarePattern(5)
+
+"""
+output
+
+*****
+*****
+*****
+*****
+*****
+
+
+"""
