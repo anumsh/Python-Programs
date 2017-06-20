88f7fed6aa33ccacda7998609fe176d381abaf20
anumsh <anumsharma09@gmail.com>
2017-06-19 22:45:23 -0700

    feat: removing  duplicate values

---
+++ removing_duplicate_values.py
@@ -0,0 +1,18 @@
+"""
+With a given list [12,24,35,24,88,120,155,88,120,155],
+write a program to print this list after removing all duplicate
+values with original order reserved.
+"""
+
+def removeDuplicate( lists):
+    newlist=[]
+    seen = set()
+    for item in lists:
+        if item not in seen:
+            seen.add( item )
+            newlist.append(item)
+
+    return newlist
+
+li=[12,24,35,24,88,120,155,88,120,155]
+print removeDuplicate(li)
