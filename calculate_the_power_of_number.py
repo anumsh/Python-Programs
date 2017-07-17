99ae2d4078c84b1b39269651eb3be8cf9343703f
anumsh <anumsharma09@gmail.com>
2017-07-16 23:46:31 -0700

    feat: Calculating the value of a to the power b

---
+++ calculate_the_power_of_number.py
@@ -0,0 +1,13 @@
+# Write a Python program to calculate the value of 'a' to the power 'b'.
+
+def power(a,b):
+	if b==0:
+		return 1
+	elif a==0:
+		return 0
+	elif b==1:
+		return a
+	else:
+		return a*power(a,b-1)
+
+print(power(2,5))
