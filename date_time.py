ce14a80b0bff8e880904e26d3b98ff8dbbc93a41
anumsh <anumsharma09@gmail.com>
2017-08-02 23:25:23 -0700

    feat: Calculate the date and time using python

---
+++ date_time.py
@@ -0,0 +1,23 @@
+"""
+Python script to display the various Date Time formats.
+
+a) Current date and time
+b) Current year
+c) Month of year
+d) Week number of the year
+e) Weekday of the week
+f) Day of year
+g) Day of the month
+h) Day of week
+"""
+
+import time
+import datetime
+print("Current date and time: " , datetime.datetime.now())
+print("Current year: ", datetime.date.today().strftime("%Y"))
+print("Month of year: ", datetime.date.today().strftime("%B"))
+print("Week number of the year: ", datetime.date.today().strftime("%W"))
+print("Weekday of the week: ", datetime.date.today().strftime("%w"))
+print("Day of year: ", datetime.date.today().strftime("%j"))
+print("Day of the month : ", datetime.date.today().strftime("%d"))
+print("Day of week: ", datetime.date.today().strftime("%A"))
