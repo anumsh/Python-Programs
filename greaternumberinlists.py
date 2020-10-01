960974b2857d1e28a00262086a2940fa3807bf32
sakshi singh <59533507+sakshi-singh-1011@users.noreply.github.com>
2020-10-01 08:34:24 +0530

    Update greaternumberinlists.py

--- greaternumberinlists.py
+++ greaternumberinlists.py
@@ -1,6 +1,4 @@
+# Program to print greatest number from a list.
 list =[3,5,6,7,17]
-max_num=list[0]
-for num in range(1,len(list)):
-  if list[num]> max_num:
-    max_num=list[num]
-print max_num
+result = max(list)
+print(result)
